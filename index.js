6352b6017be322468aaf8c9e8d68cf0e0b53f060
Pmaheshwari11 <127598258+Pmaheshwari11@users.noreply.github.com>
2023-08-02 11:37:44 +0530

    Added auto ping

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 const app = express();
 import qr from "qr-image";
 import fs from "fs";
-import path from 'path';
+import path from "path";
 const __dirname = path.resolve();
 
-app.use(express.static("Assests"))
-app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("Assests"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.render("index")
-})
+  res.render("index");
+});
 
 app.post("/", function (req, res) {
-  var url = req.body.url
+  var url = req.body.url;
   var qr_svg = qr.image(url);
-  qr_svg.pipe(fs.createWriteStream('./Assests/qr-image.png'));
-  res.render("result", { url: url })
-})
-
-
-
+  qr_svg.pipe(fs.createWriteStream("./Assests/qr-image.png"));
+  res.render("result", { url: url });
+});
 
 app.listen(process.env.PORT || 3000, function (res, req) {
-  console.log("Server started at port 3000")
-})
+  console.log("Server started at port 3000");
+});
+
+setInterval(() => {
+  axios
+    .get("https://qr-maker.onrender.com/")
+    .then((response) => {
+      console.log("Server Code " + response.status);
+    })
+    .catch((error) => console.log(error));
+}, 600000);
